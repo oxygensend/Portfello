155dfb1ff28bec032c9275395784586fd3b5e7b3
Daniel_Definski <definski@student.agh.edu.pl>
2022-01-20 17:39:32 +0100

    final version of user panel

--- project/resources/views/components/error.blade.php
+++ project/resources/views/components/error.blade.php
@@ -1,4 +1,4 @@
 @props(['name'])
 @error($name)
-    <p class="text-red-500 text-xs mt-1"> {{ $message }}</p>
+    <p class="text-red-500 text-xs mt-1 mr-2 ml-2"> {{ $message }}</p>
 @enderror
--- project/resources/views/edit-user.blade.php
+++ project/resources/views/edit-user.blade.php
@@ -1,82 +1,146 @@
 <x-app-layout>
+    <x-slot name="header">
+        User Panel
+    </x-slot>
+    <div>
+        <div class="md:grid md:grid-cols-1 md:gap-6">
+            <div class="md:col-span-1">
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+                </div>
+            </div>
+            <div class="mt-5 md:mt-0 md:col-span-2">
 
-<div>
-    <div class="md:grid md:grid-cols-3 md:gap-6">
-        <div class="md:col-span-1">
-            <div class="px-4 sm:px-0">
-                <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+                    <div class="shadow sm:rounded-md sm:overflow-hidden">
+                        <div class="p-4 bg-white space-y-6 sm:p-6 rounded-md">
+                            <div class="grid grid-cols-3 gap-6">
+                                <div class="col-span-3 sm:col-span-2">
+                                    <x-label class="block text-sm font-medium text-gray-700 mt-2">
+                                        Username
+                                    </x-label>
+                                    <form method="POST" action="/edit-user/change-username">
+                                        @csrf
+                                        @method("PATCH")
+                                        <div class="mt-1 flex ">
+                                            <input type="text" name="name" id="name" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 mr-2" value="{{$user->name}}">
+                                        </div>
+                                        <div class="mt-2 flex items-center py-2">
+                                            <x-button type="submit">
+                                                Change Username
+                                            </x-button>
+                                            <x-error name="name"/>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <div class="grid grid-cols-3 gap-6">
+                                <div class="col-span-3 sm:col-span-2">
+                                    <x-label class="block text-sm font-medium text-gray-700">
+                                        Current e-mail
+                                    </x-label>
+                                        <div class="mt-1 flex ">
+                                            <p class="mt-2 text-lg">
+                                                {{$user->email}}
+                                            </p>
+                                        </div>
+                                    <form method="POST" action="/edit-user/change-email">
+                                        @csrf
+                                        @method("PATCH")
+                                        <div class="mt-1 flex ">
+                                            <input type="text" name="email" id="email" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 mr-2" placeholder="Enter a new email address">
+                                            <input type="text" name="repeated_new_email" id="repeated_new_email" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 mr-2" placeholder="Repeat the same address">
+                                        </div>
+                                        <div class="mt-2 flex items-center py-2">
+                                            <x-button type="submit">
+                                                Change Email
+                                            </x-button>
+                                            <x-error name="email"/>
+                                            <x-error name="repeated_new_email"/>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                            <div>
+                                <x-label class="block text-sm font-medium text-gray-700">
+                                    Avatar
+                                </x-label>
+
+                                <div class="mt-1 flex items-center">
+                                        <x-image src="{{ asset($user->avatar) }}" properties="w-16 h-16 box relative" />
+                                    <form method="POST" action="/edit-user/change-avatar" enctype="multipart/form-data">
+                                        @csrf
+                                        @method("PATCH")
+                                        <x-label class="p-2" for="image" :value="__('Update Avatar ')" />
+                                        <x-input id="image" class="inline-block mt-1"
+                                                 type="file"
+                                                 name="image" />
+
+                                        <x-button type="submit">
+                                            Upload
+                                        </x-button>
+                                        <x-error name="image"/>
+                                    </form>
+                                </div>
+
+                            </div>
+                                <form method="POST" action="/edit-user/change-password">
+                                @csrf
+                                    @method("PATCH")
+                                    <div class="mt-2 flex items-center">
+
+                                        <input type="password" name="current_password" id="current_password" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md border-gray-300 mr-2" placeholder="Current password">
+                                        <input type="password" name="new_password" id="new_password" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md border-gray-300 mr-2" placeholder="New password">
+                                        <input type="password" name="repeated_new_password" id="repeated_new_password" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md border-gray-300 mr-2" placeholder="Repeat new password">
+
+                                    </div>
+                                    <div class="mt-2 flex items-center pb-2">
+                                        <x-button type="submit">
+                                            Change Password
+                                        </x-button>
+                                        <x-error name="current_password"/>
+                                        <x-error name="new_password"/>
+                                        <x-error name="repeated_new_password"/>
+                                    </div>
+                                </form>
+                        </div>
+
+                    </div>
+
             </div>
         </div>
-        <div class="mt-5 md:mt-0 md:col-span-2">
-            <form action="#" method="POST">
-                <div class="shadow sm:rounded-md sm:overflow-hidden">
-                    <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-                        <div class="grid grid-cols-3 gap-6">
-                            <div class="col-span-3 sm:col-span-2">
-                                <label for="company-website" class="block text-sm font-medium text-gray-700">
-                                    Username
-                                </label>
-                                <div class="mt-1 flex ">
-                                    <p class="mt-2 text-lg">
-                                        Tomasz Kurcaba
-                                    </p>
-                                    <button type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        Change
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="grid grid-cols-3 gap-6">
-                            <div class="col-span-3 sm:col-span-2">
-                                <label for="company-website" class="block text-sm font-medium text-gray-700">
-                                    E-mail
-                                </label>
-                                <div class="mt-1 flex ">
-                                    <p class="mt-2 text-lg">
-                                        test@test.com
-                                    </p>
-                                    <button type="button" class="ml-5 bg-white py-1 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-small text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        Change
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div>
-                            <label class="block text-sm font-medium text-gray-700">
-                                Photo
-                            </label>
-                            <div class="mt-1 flex items-center">
-                                <span class="inline-block h-16 w-16 rounded-full overflow-hidden bg-gray-100">
-                                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                                    </svg>
-                                </span>
-                                <button type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                    Change
-                                </button>
-
-                            </div>
-                        </div>
-                        <div class="mt-2 flex items-center">
-                            <button type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Change Password
-                            </button>
-                        </div>
-                    </div>
-                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <x-button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Save
-                        </x-button>
-                    </div>
-
-                </div>
-            </form>
-        </div>
     </div>
-</div>
+        <div>
+            <div class="md:grid md:grid-cols-1 md:gap-6">
+                <div class="md:col-span-1">
+                    <div class="p-4 sm:px-0 " >
+                        <h3 class="text-lg font-medium leading-6 text-gray-900">Pending invites</h3>
+                    </div>
+                </div>
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                        <div class=" sm:rounded-md sm:overflow-hidden pb-2">
+                            <div class="px-4 py-5 bg-white space-y-6 sm:p-6 p-2 rounded-md">
+                                @forelse($invites as $invite)
+                                    <div class="mt-2 flex items-center">
+                                        <strong>{{$invite->text}}</strong>
+                                        <x-action-button name="Accept"
+                                                         action="{{ route('invites.accept', $invite->id) }}" class="flex-1 mr-2 ml-2"
+                                        />
 
-    </x-app-layout>
+
+                                        <x-action-button name="Discard"
+                                                         action="{{ route('invites.delete', $invite->id)}}" class="flex-1 mr-2 ml-2"
+                                                         method='DELETE'
+                                        />
+                                    </div>
+                                @empty
+                                    <p class="p-2">U have no pending invites.</p>
+                                @endforelse
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+</x-app-layout>
