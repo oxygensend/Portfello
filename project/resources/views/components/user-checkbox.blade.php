9ff71ece112b9d5d1030c1ba9dd4b0886cf07dcb
jmachalica78 <jakubmachalica@gmail.com>
2022-01-20 17:54:39 +0100

    Adding modalbox for user selection

---
+++ project/resources/views/components/user-checkbox.blade.php
@@ -0,0 +1,7 @@
+<div class="flex w-full justify-between items-center  ">
+    <x-label for="user " class="text-lg">Tomasz abacruk </x-label>
+    <x-input  id="user" class="block ml-4"
+              type="checkbox"
+              name="description"
+              checked />
+</div>
