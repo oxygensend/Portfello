1f36afcd30891539a9f4ee3bde118f4eec6ce4ce
Tomasz Kurcaba <tomkurcaba@student.agh.edu.pl>
2022-01-23 16:08:53 +0100

    Admin can grant other user to admin role, fixing bug that user cant see user list

--- project/resources/views/groups/edit.blade.php
+++ project/resources/views/groups/edit.blade.php
@@ -4,6 +4,7 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @admin($group)
             <div class="bg-white v  overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6  bg-white border-b border-gray-200">
 
@@ -78,7 +79,7 @@
                 </div>
             </div>
         </div>
-
+        @endadmin
         <x-users-in-group-panel :users="$users" :group="$group" />
 
     </div>
