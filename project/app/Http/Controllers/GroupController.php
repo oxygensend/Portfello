b11705dde51c525f3a87c9128c37b9729e2293d9
Daniel_Definski <definski@student.agh.edu.pl>
2022-01-23 15:21:50 +0100

    adding missing line to GroupController

--- project/app/Http/Controllers/GroupController.php
+++ project/app/Http/Controllers/GroupController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
