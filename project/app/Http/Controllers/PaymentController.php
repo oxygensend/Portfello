5c3eaf8635024f89fcac28985024f3d717a231c4
Szymon Berdzik <siwy@student.agh.edu.pl>
2022-01-24 01:51:38 +0100

    Payment validation. Why don't dinosaurs talk? becouse they are dead'

--- project/app/Http/Controllers/PaymentController.php
+++ project/app/Http/Controllers/PaymentController.php
@@ -13,27 +13,28 @@ use Illuminate\Validation\ValidationException;
 use phpDocumentor\Reflection\DocBlock\Tags\Author;
 use function PHPUnit\Framework\at;
 
-class PaymentController extends Controller
-{
+class PaymentController extends Controller {
+
     //
 
 
-    public function create(Group $group){
+    public function create(Group $group)
+    {
 
-        return view('payments.create',['group'=>$group]);
+        return view('payments.create', ['group' => $group]);
 
     }
 
 
+    public function getItemsList(Group $group, User $user)
+    {
 
-    public function getItemsList(Group $group, User $user){
 
-
-       $items= \Auth::user()->getItemBalanceWithUser($user, $group);
+        $items = \Auth::user()->getItemBalanceWithUser($user, $group);
 //todo
-        $filtered=[];
-        foreach($items  as $key => $value){
-            if($value<0) $filtered[$key]=$value;
+        $filtered = [];
+        foreach ($items as $key => $value) {
+            if ($value < 0) $filtered[$key] = $value;
         }
 
         return json_encode($filtered);
@@ -41,24 +42,33 @@ class PaymentController extends Controller
     }
 
 
-
-    public function store(Group $group){
+    public function store(Group $group)
+    {
 
         $user = auth()->user();
 
+        $attributes = request()->validate([
+            'select_user' => 'required:in:' . $user->whomOwe($group),
+            'item_select' => 'nullable',
+            'how_much' => 'required|numeric|min:0.01',
+        ]);
 
-            $attributes = request()->validate([
-                'select_user' => 'required:in:' . $user->whomOwe($group),
-                'item_select' => 'nullable',
-                'how_much' => 'required|numeric|min:0.01'
-            ]);
 
+        $user2 = User::find($attributes['select_user']);
+        if (\request()->how == 'item' && !(\request()->item_select ?? null)) {
 
-            $user2 = User::find($attributes['select_user']);
-            if($attributes['how_much'] > abs($user->getBalanceWithUser($user2, $group)) && ($attributes['item_select']??null) == null){
-                throw ValidationException::withMessages(['how_much'=>'Too large payment amount']);
-            }
+            throw ValidationException::withMessages(['how_much' => 'You owe no items to this user']);
+        }
+        if ($attributes['how_much'] > abs($user->getBalanceWithUser($user2, $group)) && !($attributes['item_select'] ?? null)) {
+            throw ValidationException::withMessages(['how_much' => 'Too large payment amount']);
+        }
 
+        if ($attributes['item_select'] ?? null)
+            $item_max = abs($user->getItemBalanceWithUser($user2, $group)[$attributes['item_select']]);
+        if (($attributes['item_select'] ?? null) && $attributes['how_much'] > $item_max) {
+
+            throw ValidationException::withMessages(['how_much' => 'Too large payment amount for this item']);
+        }
 
 
         Payment::create([
@@ -66,11 +76,11 @@ class PaymentController extends Controller
             'user_2_id' => $attributes['select_user'],
             'item' => ($attributes['item_select'] ?? null),
             'amount' => $attributes['how_much'],
-            'group_id' => $group->id
+            'group_id' => $group->id,
         ]);
 
 
-        return redirect(route('groups.show', $group))->with('success','Payment done');
+        return redirect(route('groups.show', $group))->with('success', 'Payment done');
 
     }
 }
