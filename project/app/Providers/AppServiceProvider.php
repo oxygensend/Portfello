19c2a252248ebfb528d8b84a1e6a5c1a30926f89
Szymon Berdzik <szymon19314@wp.pl>
2022-03-28 23:11:27 +0200

    FIXING css

--- project/app/Providers/AppServiceProvider.php
+++ project/app/Providers/AppServiceProvider.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
+
     /**
      * Register any application services.
      *
@@ -25,19 +25,23 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
+
     {
-        Gate::define('admin', function ($user,$group) {
-            return  $user->id == $group->admin->id;
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+
+        Gate::define('admin', function ($user, $group) {
+            return $user->id == $group->admin->id;
         });
 
-        Gate::define('member', function ($user,$group) {
-            foreach ($group->users()->get() as $user_in_group){
-                if($user->id == $user_in_group->id)
+        Gate::define('member', function ($user, $group) {
+            foreach ($group->users()->get() as $user_in_group) {
+                if ($user->id == $user_in_group->id)
                     return true;
             }
             return false;
         });
-
 
 
         Blade::if('admin', function ($group) {
@@ -49,11 +53,11 @@ class AppServiceProvider extends ServiceProvider
         });
 
         Blade::if('expense_creator', function ($expense) {
-            return request()->user()==$expense->user &&  $expense->isLatest == true && $expense->action !=3;
+            return request()->user() == $expense->user && $expense->isLatest == true && $expense->action != 3;
         });
 
-        Gate::define('expense_creator', function ($user,$expense) {
-            return $user==$expense->user &&  $expense->isLatest == true && $expense->action !=3;
+        Gate::define('expense_creator', function ($user, $expense) {
+            return $user == $expense->user && $expense->isLatest == true && $expense->action != 3;
         });
 
         Model::unguard();
